d4b14f1202251d59eb0a38de5fe6987fb69b73d4
Chathurvedhi <chathurvedhitalapaneni@gmail.com>
2023-04-24 06:30:22 +0530

    LSA works, clear doubt before Dijkstra

--- A5/ospf.cpp
+++ A5/ospf.cpp
@@ -20,6 +20,13 @@ public:
     int max_cost;
 };
 
+class lsa_info
+{
+public:
+    int seqno;
+    unordered_map<int, int> entries;
+};
+
 class ospf
 {
 public:
@@ -28,21 +35,68 @@ public:
     int lsa_interval = 5;
     int spf_interval = 5;
     int sockfd;
+    int lsa_seqno = 1;
     struct sockaddr_in servaddr;
     struct sockaddr_in cliaddr;
     struct sockaddr_in neighbour_addr;
     int len;
     unordered_map<int, file_info> neighbours_file;
     unordered_map<int, int> neighbours_cost;
+    unordered_map<int, int> lsa_nodes;
+    vector<vector<int>> topology_table;
+    unordered_map<int, lsa_info> lsa_table;
     void ospf_init(string filename);
     void reciever();
     void hello_response();
     void helloreply_response();
-    void lsa_response();
+    void lsa_response(string buffer);
     void hello_gen();
     void lsa_gen();
     void topology();
 };
+
+void ospf::lsa_response(string buffer)
+{
+    // Sender details
+    char* sender_id;
+    sender_id = strtok(NULL, "|");
+    int n_id = atoi(sender_id);
+    char* sender_seqno;
+    sender_seqno = strtok(NULL, "|");
+    int n_seqno = atoi(sender_seqno);
+
+    // Check Sequence number
+    if(lsa_nodes[n_id] >= n_seqno) return;
+    lsa_nodes[n_id] = n_seqno;
+
+    // Update topology table
+    int num_entries;
+    char* num_entries_str;
+    num_entries_str = strtok(NULL, "|");
+    num_entries = atoi(num_entries_str);
+
+    for(int i=0; i<num_entries; i++)
+    {
+        char* entry;
+        entry = strtok(NULL, "|");
+        int entry_id = atoi(entry);
+        entry = strtok(NULL, "|");
+        int entry_cost = atoi(entry);
+        topology_table[n_id][entry_id] = entry_cost;
+        topology_table[entry_id][n_id] = entry_cost;
+    }
+
+    // Send LSA to all neighbours
+    for(auto it = neighbours_file.begin(); it != neighbours_file.end(); it++)
+    {
+        if(it->first == n_id) continue;
+        // Send to port 10000 + it->first
+        neighbour_addr.sin_family = AF_INET;
+        neighbour_addr.sin_port = htons(10000 + it->first);
+        neighbour_addr.sin_addr.s_addr = inet_addr(localhost);
+        sendto(sockfd, (const char *)buffer.c_str(), strlen(buffer.c_str()), MSG_CONFIRM, (const struct sockaddr *)&neighbour_addr, len);
+    }
+}
 
 void ospf::hello_response()
 {
@@ -104,10 +158,12 @@ void ospf::reciever()
         char buffer[MAX_LINE];
         int n = recvfrom(sockfd, (char *)buffer, MAX_LINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, (socklen_t *)&len);
         buffer[n] = '\0';
+        string packet = buffer;
         char* pack_type;
         pack_type = strtok(buffer, "|");
         if(strcmp(pack_type, "HELLO")==0) hello_response();
         else if(strcmp(pack_type, "HELLOREPLY")==0) helloreply_response();
+        else if(strcmp(pack_type, "LSA")==0) lsa_response(packet);
     }
 }
 
@@ -116,19 +172,55 @@ void ospf::hello_gen()
     while(1)
     {
         sleep(hello_interval);
+        char hello[MAX_LINE];
+        strcpy(hello, "HELLO|");
+        strcat(hello, to_string(id).c_str());
+
+        // Send to all neighbours
         for(auto it = neighbours_file.begin(); it != neighbours_file.end(); it++)
         {
-            char hello[MAX_LINE];
-            strcpy(hello, "HELLO|");
-            strcat(hello, to_string(id).c_str());
-            strcat(hello, "|");
-            strcat(hello, to_string(it->first).c_str());
             // Send to port 10000 + it->first
             neighbour_addr.sin_family = AF_INET;
             neighbour_addr.sin_port = htons(10000 + it->first);
             neighbour_addr.sin_addr.s_addr = inet_addr(localhost);
             sendto(sockfd, (const char *)hello, strlen(hello), MSG_CONFIRM, (const struct sockaddr *)&neighbour_addr, len);
         }
+    }
+}
+
+void ospf::lsa_gen()
+{
+    while(1)
+    {
+        sleep(lsa_interval);
+        string lsa;
+        lsa = "LSA|";
+        lsa += to_string(id);
+        lsa += "|";
+        lsa += to_string(lsa_seqno);
+        lsa += "|";
+        int num_neighbours = neighbours_cost.size();
+        lsa += to_string(num_neighbours);
+        lsa += "|";
+        for(auto it = neighbours_cost.begin(); it != neighbours_cost.end(); it++)
+        {
+            lsa += to_string(it->first);
+            lsa += "|";
+            lsa += to_string(it->second);
+            lsa += "|";
+        }
+        
+        // Send to all neighbours
+        for(auto it = neighbours_file.begin(); it != neighbours_file.end(); it++)
+        {
+            // cout<< " Sending LSA to " << it->first << endl;
+            // Send to port 10000 + it->first
+            neighbour_addr.sin_family = AF_INET;
+            neighbour_addr.sin_port = htons(10000 + it->first);
+            neighbour_addr.sin_addr.s_addr = inet_addr(localhost);
+            sendto(sockfd, (const char *)lsa.c_str(), strlen(lsa.c_str()), MSG_CONFIRM, (const struct sockaddr *)&neighbour_addr, len);
+        }
+        lsa_seqno++;
     }
 }
 
@@ -140,12 +232,22 @@ void ospf::topology()
         fstream fp;
         fp.open("Outputs/output_" + to_string(id), ios::out);
         fp << "Node " << id << " : " << endl;
-        fp << "Neighbours : " << endl;
+        
+        // Print a Adjaency Matrix
+        fp << "Adjacency Matrix : " << endl;
+        for(int i = 0; i < topology_table.size(); i++)
+        {
+            for(int j = 0; j < topology_table[i].size(); j++)
+            {
+                fp << topology_table[i][j] << " ";
+            }
+            fp << endl;
+        }
+        fp << "Neighbours :" << endl;
         for(auto it = neighbours_cost.begin(); it != neighbours_cost.end(); it++)
         {
             fp << it->first << " " << it->second << endl;
         }
-        fp.close();
     }
 }
 
@@ -158,6 +260,17 @@ void ospf::ospf_init(string filename)
     fp >> num_nodes;
     int num_edges;
     fp >> num_edges;
+
+    // initialise adjacency matrix
+    for(int i = 0; i < num_nodes; i++)
+    {
+        vector<int> temp;
+        for(int j = 0; j < num_nodes; j++)
+        {
+            temp.push_back(0);
+        }
+        topology_table.push_back(temp);
+    }
 
     // Read lines to get all neighbours for id
     for(int i = 0; i < num_edges; i++)
@@ -206,10 +319,12 @@ void ospf::ospf_init(string filename)
     thread ospf_reciever(&ospf::reciever, this);
     thread ospf_hello_gen(&ospf::hello_gen, this);
     thread ospf_topology(&ospf::topology, this);
+    thread ospf_lsa_gen(&ospf::lsa_gen, this);
 
     ospf_reciever.join();
     ospf_hello_gen.join();
     ospf_topology.join();
+    ospf_lsa_gen.join();
 }
 
 int main(int argc, char *argv[])
